4cc70cfbd2e162e328b1ca877e9ed9e85b20831c
danilokovacs <danilo.kovacs@bandtec.com.br>
2022-10-19 15:17:02 -0300

    wip: adjusting css to modules

--- ido/src/components/Home/FilterPesquisar.jsx
+++ ido/src/components/Home/FilterPesquisar.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import iconLupa from '../../assets/images/icon-lupa.png';
-
 import style from './Home.module.css';
 
 export default function FilterPesquisar({openModal, openModalVerTarefa}){
 
-    const opacidade = (openModal || openModalVerTarefa) ? ".opacidade_model": "";
+    const opacidade = (openModal || openModalVerTarefa) ? style.opacidade_model : "";
 
     return(
         <div className={style.pesquisar_container}>
             <div id="elementoPesquisar" className={style.elemento_pesquisar}>
                 <input id="inputPesquisar" className={style.input_pesquisar} type="text" placeholder="Pesquisar"/>
-                <img id="iconLupa"  className={[`${style.icon_lupa} ${style + opacidade}`]} src={iconLupa} alt="barra de pesquisa"/>
+                <img id="iconLupa"  className={`${style.icon_lupa} ${opacidade}`} src={iconLupa} alt="barra de pesquisa"/>
             </div>
         </div>
     );
