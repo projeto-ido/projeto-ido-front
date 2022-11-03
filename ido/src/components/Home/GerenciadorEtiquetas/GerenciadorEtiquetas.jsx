64513a594b297e066b1239c6bbe73005acb54ae5
danilokovacs <danilo.kovacs@bandtec.com.br>
2022-11-03 20:40:48 -0300

    construção do modal de etiquetas

--- ido/src/components/Home/GerenciadorEtiquetas/GerenciadorEtiquetas.jsx
+++ ido/src/components/Home/GerenciadorEtiquetas/GerenciadorEtiquetas.jsx
@@ -1,56 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./GerenciadorEtiquetas.module.css";
-import iconAdicionar from "../../../assets/images/icon-adicionar.png"
-import iconLixeira from "../../../assets/images/icon-lixeira.png"
+import iconAdicionar from "../../../assets/images/icon-adicionar.png";
+import iconLixeira from "../../../assets/images/icon-lixeira.png";
 import Etiqueta from "./Etiqueta";
 import ModalCriarEtiqueta from "./ModalCriarEtiqueta";
+import GerenciamentoEtiqueta from "./GerenciamentoEtiqueta";
 
-function GerenciadorEtiquetas(){
-    
-    return(
-        <>
-           <div className={style.fundo_escuro} >
-               <div className={style.modal_lateral}>
-                   <div className={style.container_texto_destaque}>
-                        <span className={style.texto_destaque}>GERENCIADOR DE ETIQUETAS</span>
-                   </div>
+function GerenciadorEtiquetas() {
+  const [openModalEtiqueta, setOpenModalEtiqueta] = useState(false);
+  const [textoInput, setTextoInput] = useState("")
 
-                    <div className={style.container_acoes}>
-                        <h2>Lista de etiquetas</h2>
-                        <div className={style.container_acoes_icons}>
-                            <img src={iconLixeira} className={style.acoes} alt="icone lixeira" />
-                            <img src={iconAdicionar} className={style.acoes} alt="icone adicionar" />
-                        </div>
-                    </div>
+  return (
+    <>
+      <div className={style.fundo_escuro}>
+        <ModalCriarEtiqueta
+          openModalEtiqueta={openModalEtiqueta}
+          setOpenModalEtiqueta={setOpenModalEtiqueta}
+          textoInput={textoInput}
+          setTextoInput={setTextoInput}
+        />
 
-                    <div className={style.listagem_etiquetas}>
-                        <Etiqueta titulo="Facul" cor="#7463C7"/>
-                        <Etiqueta titulo="Facul" cor="#7463C7"/>
-                        <Etiqueta titulo="Facul" cor="#7463C7"/>
-                        <Etiqueta titulo="Facul" cor="#7463C7"/>
-                        <Etiqueta titulo="Facul" cor="#7463C7"/>
-                        <Etiqueta titulo="Facul" cor="#7463C7"/>
-                        <Etiqueta titulo="Facul" cor="#7463C7"/>
-                        <Etiqueta titulo="Facul" cor="#7463C7"/>
-                        <Etiqueta titulo="Facul" cor="#7463C7"/>
-                        <Etiqueta titulo="Facul" cor="#7463C7"/>
-                        <Etiqueta titulo="Facul" cor="#7463C7"/>
-                        <Etiqueta titulo="Facul" cor="#7463C7"/>
-                        <Etiqueta titulo="Facul" cor="#7463C7"/>
-                        <Etiqueta titulo="Facul" cor="#7463C7"/>
-                        <Etiqueta titulo="Facul" cor="#7463C7"/>
-                        <Etiqueta titulo="Facul" cor="#7463C7"/>
-                        <Etiqueta titulo="Facul" cor="#7463C7"/>
-                        <Etiqueta titulo="Facul" cor="#7463C7"/>
-                        <Etiqueta titulo="Facul" cor="#7463C7"/>
-                        <Etiqueta titulo="Facul" cor="#7463C7"/>
-                    </div>
-               </div>
+        <div className={style.modal_lateral}>
+          <div className={style.container_texto_destaque}>
+            <span className={style.texto_destaque}>
+              GERENCIADOR DE ETIQUETAS
+            </span>
+          </div>
 
-               <ModalCriarEtiqueta />
-           </div>
-        </>
-    );
+          <div className={style.container_acoes}>
+            <h2>Lista de etiquetas</h2>
+            <div className={style.container_acoes_icons}>
+              <img
+                src={iconLixeira}
+                className={style.acoes}
+                alt="icone lixeira"
+              />
+              <img
+                src={iconAdicionar}
+                className={style.acoes}
+                alt="icone adicionar"
+              />
+            </div>
+          </div>
+
+          <div className={style.listagem_etiquetas}>
+            <GerenciamentoEtiqueta titulo="Facul" cor="#7463C7" />
+            <GerenciamentoEtiqueta titulo="Facul" cor="#7463C7" />
+            <GerenciamentoEtiqueta titulo="Facul" cor="#7463C7" />
+            <GerenciamentoEtiqueta titulo="Facul" cor="#7463C7" />
+            <GerenciamentoEtiqueta titulo="Facul" cor="#7463C7" />
+            <GerenciamentoEtiqueta titulo="Facul" cor="#7463C7" />
+            <GerenciamentoEtiqueta titulo="Facul" cor="#7463C7" />
+            <GerenciamentoEtiqueta titulo="Facul" cor="#7463C7" />
+            <GerenciamentoEtiqueta titulo="Facul" cor="#7463C7" />
+            <GerenciamentoEtiqueta titulo="Facul" cor="#7463C7" />
+            <GerenciamentoEtiqueta titulo="Facul" cor="#7463C7" />
+            <GerenciamentoEtiqueta titulo="Facul" cor="#7463C7" />
+            <GerenciamentoEtiqueta titulo="Facul" cor="#7463C7" />
+            <GerenciamentoEtiqueta titulo="Facul" cor="#7463C7" />
+            <GerenciamentoEtiqueta titulo="Facul" cor="#7463C7" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default GerenciadorEtiquetas
+export default GerenciadorEtiquetas;
---
+++ ido/src/components/Home/GerenciadorEtiquetas/GerenciamentoEtiqueta.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import style from "./GerenciadorEtiquetas.module.css";
+import iconEditar from "../../../assets/images/icon-editar-etiqueta.png";
+import Etiqueta from "./Etiqueta";
+
+function GerenciamentoEtiqueta({ titulo, cor }) {
+  return (
+    <>
+      <div className={style.container_etiqueta_acoes}>
+        <Etiqueta titulo={titulo} cor={cor} />
+        <div className={style.container_acoes}>
+          <img className={style.acoes} src={iconEditar} alt="icone lapis" />
+          <input className={style.checkbox_etiqueta} type="checkbox" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default GerenciamentoEtiqueta;
