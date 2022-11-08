9fe7e5414e83fac6f3bca417e4d30df16c102eb8
danielamrl <daniel.silva@bandtec.com.br>
2022-11-08 03:12:52 -0300

    Atualizando estrutura de decisão da acessibilidade

--- ido/src/pages/SiteInstitucional/SiteInstitucional.jsx
+++ ido/src/pages/SiteInstitucional/SiteInstitucional.jsx
@@ -30,13 +30,20 @@ function SiteInstitucional(){
         document.body.style.setProperty('--cor--roxo', '#1CA0C7');
         document.body.style.setProperty('--cor--amarela', '#FFCA6D');
 
-    } else {
+    } else if (sessionStorage.getItem("name") === "Tritanopia") {
         /* TRITANOPIA */
         document.body.style.setProperty('--cor--azul', '#95F313');
         document.body.style.setProperty('--cor--verde', '#238FF7');
         document.body.style.setProperty('--cor--roxo', '#FFA58D');
         document.body.style.setProperty('--cor--amarela', '#E44D4D');
 
+    } else {
+        /* NORMAL */
+        document.body.style.setProperty('--cor--azul', '#5D84C2');
+        document.body.style.setProperty('--cor--verde', '#51BDAB');
+        document.body.style.setProperty('--cor--roxo', '#7463C7');
+        document.body.style.setProperty('--cor--amarela', '#FFCA6D');
+        
     }
 
     return(
