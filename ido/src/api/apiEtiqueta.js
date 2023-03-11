dcfe6a275ca4bbfded52bae5a74db4eb252cfa56
danilokovacs <danilo.kovacs@bandtec.com.br>
2023-03-10 21:17:09 -0300

    alterando url para requisicoes no backend na aws

--- ido/src/api/apiEtiqueta.js
+++ ido/src/api/apiEtiqueta.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiEtiqueta = axios.create({
-  baseURL: "https://6336071965d1e8ef266747fb.mockapi.io/grafico-etiqueta"
+  baseURL: "http://35.171.74.239:8080/usuarios/perfil/grafico-etiqueta"
 })
 
 export default apiEtiqueta;
--- ido/src/api/apiService.js
+++ ido/src/api/apiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiService = axios.create({
-  baseURL: "http://localhost:8080"
+  baseURL: "http://35.171.74.239:8080"
 })
 
 export default apiService;
